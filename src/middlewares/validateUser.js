fd14f07d547c5705e45038d28f304b247262a71e
Yeiner Parra <yeinerparrabernal@gmail.com>
2025-01-02 10:04:10 -0500

    feat/ se crean las validaciones para los usuarios

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,4 +1,39 @@
 import { body, validationResult } from "express-validator";
 import AppError from "../utils/appError";
 
-export const validateUser = [];
+export const validateUser = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("status")
+    .optional()
+    .isIn(["active", "disable"])
+    .withMessage("Status is invalid"),
+  body("role")
+    .optional()
+    .isIn(["admin", "user"])
+    .withMessage("Role is invalid"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(
+        new AppError(
+          errors
+            .array()
+            .map((err) => err.message)
+            .join(", "),
+          400
+        )
+      );
+    }
+    next();
+  },
+];
