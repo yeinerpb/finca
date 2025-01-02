fecf7be41fc0dd1630380b11a0583f10087eb50d
Yeiner Parra <yeinerparrabernal@gmail.com>
2025-01-02 17:21:15 -0500

    feat: se agregan las rutas para usuarios

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { validateUser } from "../middlewares/validateUser.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import {
+  createUser,
+  loginUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController";
+
+const router = express.Router();
+
+//public routes
+router.post("/users", validateUser, createUser);
+router.post("/login", loginUser);
+
+//private routes
+router.use(protect);
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
